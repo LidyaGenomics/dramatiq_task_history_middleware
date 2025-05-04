0519340fe0ee25ece8b31a3ac0ff69ba93fafb0c
Ahmet Can Ogreten <ahmetcan@lidyagenomics.com>
2025-05-04 22:13:59 +0300

    initial files for python package requirement

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dramatiq_task_history_middleware",
+    version="0.1.0",
+    package_dir={"": "dramatiq_task_history_middleware"},
+    packages=find_packages(where="dramatiq_task_history_middleware"),
+    install_requires=[
+        "django>=4.2.0",
+        "dramatiq>=1.12.0",
+    ],
+    author="Ahmet Can Ogreten",
+    author_email="ahmetcan@lidyagenomics.com",
+    description="A dramatiq task history middleware package for Lidya",
+    keywords="dramatiq, task, history, middleware",
+    url="https://github.com/LidyaGenomics/dramatiq_task_history_middleware.git",
+    python_requires=">=3.10",
+) 
