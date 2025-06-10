6dc057e5808e1ea80700bf6f61ece4936d6aa1bc
Ahmet Can Ogreten <ahmetcan@lidyagenomics.com>
2025-06-10 11:54:39 +0300

    started_at and completed_at fields added to pipeline api

--- dramatiq_task_history_middleware/dramatiq_task_history_middleware/serializers.py
+++ dramatiq_task_history_middleware/dramatiq_task_history_middleware/serializers.py
@@ -9,7 +9,21 @@ class TaskSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class PipelineSerializer(serializers.ModelSerializer):
+    started_at = serializers.SerializerMethodField()
+    completed_at = serializers.SerializerMethodField()
     
     class Meta:
         model = Pipeline
         fields = '__all__'
+    
+    def get_started_at(self, obj):
+        """Return the earliest enqueued_at time from pipeline tasks"""
+        earliest_task = obj.task_set.order_by('enqueued_at').first()
+        return earliest_task.enqueued_at if earliest_task else None
+    
+    def get_completed_at(self, obj):
+        """Return the latest completed_at time if pipeline status is failed/success"""
+        if obj.status in ['failed', 'success']:
+            latest_task = obj.task_set.filter(completed_at__isnull=False).order_by('-completed_at').first()
+            return latest_task.completed_at if latest_task else None
+        return None
